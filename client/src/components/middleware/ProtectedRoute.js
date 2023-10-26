6f4bf1f81769dd36646fa515cbfec5cf42c2abc2
unknown <sreenivas@techjet.ai>
2023-10-26 11:34:24 +0530

    build

--- client/src/components/middleware/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom"
-
-const ProtectedRoute = (props) => (
-    props.auth === true ? props.children : <Navigate replace to={`/`} />
-)
-
-
-
-export default ProtectedRoute
