b590a666c180661d7177aa244713e7e61e01a8a4
unknown <sreenivas@techjet.ai>
2023-11-10 17:27:43 +0530

    none

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const cors = require('cors');
+// const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const path = require("path");
-// const moragan = require("morgan");
+const moragan = require("morgan");
 
 
 
@@ -19,8 +19,8 @@ const meetingRoute = require('./route/meetingRoute')
 app.use(cors());
 app.use(cookieParser(process.env.REF_TOKEN_SECRET));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// app.use(moragan("dev"));
+// app.use(express.json());
+app.use(moragan("dev"));
 
 
 app.use(`/api/v1/auth`, authRoute)
@@ -29,13 +29,13 @@ app.use(`/api/v1/deal`, dealRoute)
 app.use(`/api/v1/event`, meetingRoute)
 
 
-// app.use('/public/Images', express.static(path.join(__dirname, 'public/Images')));
+app.use('/public/Images', express.static(path.join(__dirname, 'public/Images')));
 
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 
 const start = async () => {
