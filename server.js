45ca9a0fc51afe826186d983c7749ba827451f0e
unknown <sreenivas@techjet.ai>
2023-11-15 16:18:24 +0530

    dd

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 const app = express();
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
+app.set('./views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 const PORT = process.env.PORT || 7000;
 const connectDB = require("./db");
